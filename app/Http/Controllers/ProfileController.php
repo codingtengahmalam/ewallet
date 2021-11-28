0137621d1a916c468de32fbcf69a8f6b40b5f4ff
hellodit <asditap@gmail.com>
2021-11-28 23:20:59 +0700

    create MVC pattern

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Wallet;
 use App\Repositories\UserRepository;
 use App\Repositories\WalletRepository;
 use Illuminate\Http\Request;
@@ -25,12 +26,22 @@ class ProfileController extends Controller
      */
     public function profile()
     {
-        $user = $this->urepo->getById(auth()->user()->id);
+        $user  = User::whereId(auth()->user()->id)->with(['wallet' => function($wallet){
+            return $wallet->select('id','balance','status','user_id');
+        }])->firstOrFail();
+
         return $this->responseSuccess("Success",$user);
     }
 
     public function registerWallet(){
-        $wallet = $this->wrepo->getOrCreateWallet(auth()->user()->id);
+
+        $wallet = Wallet::firstOrCreate([
+            'user_id' => auth()->user()->id
+        ],[
+            'balance' => 0,
+            'status' => Wallet::STATUS_ACTIVE,
+        ]);
+
         return $this->responseSuccess("Success", $wallet);
     }
 }
--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
+use App\Models\Wallet;
 use App\Repositories\UserRepository;
 use App\Repositories\WalletRepository;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class WalletController extends Controller
 {
@@ -17,7 +20,11 @@ class WalletController extends Controller
 
     public function getDetail(){
         $wallet_id = auth()->user()->wallet->id;
-        $detail = $this->wrepo->getWalletDetail($wallet_id);
+
+        $detail = Wallet::whereId($wallet_id)->with(['transactions' => function($trs){
+            return $trs->orderBy('created_at','desc')->limit(5);
+        }])->firstOrFail();
+
         return $this->responseSuccess('success', $detail);
     }
 
@@ -28,7 +35,30 @@ class WalletController extends Controller
         ]);
 
         $wallet_id = auth()->user()->wallet->id;
-        $wallet = $this->wrepo->increaseBalance($wallet_id,$request->amount,'topup balance');
+        $amount = $request->amount;
+        $note ='topup balance';
+
+        $wallet = \DB::transaction(function () use ($wallet_id, $amount,$note) {
+            $wallet = Wallet::whereId($wallet_id)->first();
+            if (empty($wallet)){
+                throw ValidationException::withMessages([
+                    'wallet' => 'Wallet id not found'
+                ]);
+            }
+
+            $wallet->balance += $amount;
+            $wallet->update();
+
+            $trs = New Transaction();
+            $trs->type = Transaction::TYPE_CREDIT;
+            $trs->amount = $amount;
+            $trs->description = $note;
+            $trs->wallet_id = $wallet->id;
+            $trs->save();
+
+            return $wallet;
+        });
+
         return $this->responseSuccess('success topup wallet', $wallet);
     }
 
@@ -39,8 +69,55 @@ class WalletController extends Controller
             'note' => 'string|max:100'
         ]);
 
-        $result = $this->wrepo->transferBalance($request->wallet_id, $request->amount, $request->note);
 
-       return $this->responseSuccess('Success transfer balance', $result);
+        $wallet_id = $request->wallet_id;
+        $amount =  $request->amount;
+        $note = $request->note;
+
+        $result =  \DB::transaction(function () use ($wallet_id, $amount, $note) {
+            $source = auth()->user()->wallet();
+
+            if($source->balance < $amount) {
+                throw new \Exception("your balance is not sufficient");
+            }
+
+            $source->balance -= $amount;
+            $source->update();
+
+            //new record decrease balance
+            $source_history = New Transaction();
+            $source_history->type = Transaction::TYPE_DEBIT;
+            $source_history->amount = $amount;
+            $source_history->description = $note;
+            $source_history->wallet_id = $source->id;
+            $source_history->save();
+
+
+            $target = Wallet::whereId($wallet_id)->first();
+            if (empty($wallet)){
+                throw ValidationException::withMessages([
+                    'wallet' => 'Wallet id not found'
+                ]);
+            }
+            if ($target->status != Wallet::STATUS_ACTIVE){
+                throw new \Exception("Wallet not active");
+            }
+
+            $target->balance += $amount;
+            $target->update();
+
+            //new record decrease balance
+            $target_history = New Transaction();
+            $target_history->type = Transaction::TYPE_CREDIT;
+            $target_history->amount = $amount;
+            $target_history->description = $note;
+            $target_history->wallet_id = $target->id;
+            $target_history->save();
+
+            return $source;
+        });
+
+
+        return $this->responseSuccess('Success transfer balance', $result);
     }
 }
